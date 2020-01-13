bfbbb191ce13a0c15100c803af10ecf7f48a6008
Philip Giannioudis <giannioudis@gmail.com>
2020-01-13 10:18:12 -0500

    Double Linked List

---
+++ Algo-CSharp/DataStructures/Lists/LinkList/DoublyLinkedList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Interview_Review.DataStructures.Lists.LinkList
+{
+    public class DoublyLinkedList<T>
+    {
+        public DoublyLinkedNode<T> Head { get; private set; }
+        public DoublyLinkedNode<T> Tail { get; private set; }
+        public bool IsEmpty => Count == 0;
+        public int Count { get; set; }
+
+        public void AddFirst(T value)
+        {
+            AddFirst(new DoublyLinkedNode<T>(value));
+        }
+
+        private void AddFirst(DoublyLinkedNode<T> node)
+        {
+            // Save off the Head
+            DoublyLinkedNode<T> temp = Head;
+
+            // Point head to node
+            Head = node;
+
+            // insert the rest of the list behind head
+            Head.Next = temp;
+
+            if (IsEmpty)
+            {
+                Tail = Head;
+            }
+            else
+            {
+                temp.Previous = Head;
+            }
+
+            Count++;
+        }
+
+        public void AddLast(T value)
+        {
+            AddLast(new DoublyLinkedNode<T>(value));
+        }
+
+        private void AddLast(DoublyLinkedNode<T> node)
+        {
+            if (IsEmpty)
+            {
+                Head = node;
+            } else
+            {
+                Tail.Next = node;
+                node.Previous = Tail;
+            }
+
+            Tail = node;
+            Count++;
+        }
+
+        public void RemoveFirst()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException();
+            }
+
+            Head = Head.Next;
+            Count--;
+            if (IsEmpty)
+            {
+                Tail = null;
+            } else
+            {
+                Head.Previous = null;
+            }
+        }
+
+        public void RemoveLast()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if(Count == 1)
+            {
+                Head = null;
+                Tail = null;
+            } else
+            {
+                Tail.Previous.Next = null; // Null Last Node
+                Tail = Tail.Previous; // Shift the Tail
+            }
+
+            Count--;
+        }
+    }
+}
---
+++ Algo-CSharp/DataStructures/Lists/LinkList/DoublyLinkedNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Review.DataStructures.Lists.LinkList
+{
+    public class DoublyLinkedNode<T>
+    {
+        public T Value { get; set; }
+        public DoublyLinkedNode<T> Next { get; set; }
+        public DoublyLinkedNode<T> Previous { get; set; }
+
+        public DoublyLinkedNode(T value)
+        {
+            this.Value = value;
+        }
+    }
+}
